65cbf114ca1aced9fd2731282e3550dac3a7d13d
IrinaZhit <165552692+IrinaZhit@users.noreply.github.com>
2024-10-13 18:57:16 +0300

    Create 5.java

---
+++ Классы и методы/Базовые типы данных и литералы/5.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.5;
+        double result = a + b;
+        System.out.println(result); // Вывод: 7.5
+    }
+} //При сложении int и double результатом будет double. int автоматически преобразуется в double для выполнения операции.
