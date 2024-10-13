69b3b94f86abacd662be6026a53865550087392e
IrinaZhit <165552692+IrinaZhit@users.noreply.github.com>
2024-10-13 20:02:10 +0300

    Update 8.java

--- Классы и методы/Базовые типы данных и литералы/8.java
+++ Классы и методы/Базовые типы данных и литералы/8.java
@@ -1,18 +1,18 @@
-// Интерфейс
-interface Животное {
-    void издатьЗвук();
+//Интерфейс
+interface Animal {
+    void makeSound();
 }
 
-// Реализация интерфейса
-class Собака implements Животное {
-    public void издатьЗвук() {
-        System.out.println("Гав!");
+//Реализация интерфейса
+class Dog implements Animal {
+    public void makeSound() {
+        System.out.println("Bark!");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        var собака = new Собака(); // использование var
-        собака.издатьЗвук(); // Вывод: Гав!
+        var dog = new Dog(); // использование var
+        dog.makeSound(); //Вывод: Bark!
     }
 } // интерфейс Животное, реализовали его в классе Собака и использовали var для создания объекта.
