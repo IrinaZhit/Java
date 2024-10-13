3db410563f918a0302c573ad1b2438760d3d836c
IrinaZhit <165552692+IrinaZhit@users.noreply.github.com>
2024-10-13 18:58:39 +0300

    Create 6.java

---
+++ Классы и методы/Базовые типы данных и литералы/6.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 9.99;
+        int b = (int) a; // Сужающее преобразование
+        System.out.println(b); // Вывод: 9
+    }
+}
