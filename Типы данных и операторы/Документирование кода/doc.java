19fa408e35fc9de013b92cb37265fa9077ba8a4f
IrinaZhit <165552692+IrinaZhit@users.noreply.github.com>
2024-10-13 20:15:30 +0300

    Update and rename Типы данных и операторы/Документирование кода.txt to Типы данных и операторы/Документирование кода/doc.java

---
+++ Типы данных и операторы/Документирование кода/doc.java
@@ -0,0 +1,15 @@
+/**
+ * Класс Calculator предоставляет базовые арифметические операции.
+ */
+class Calculator {
+    
+    /**
+     * Складывает два целых числа.
+     * @param a первое целое число
+     * @param b второе целое число
+     * @return сумма a и b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
