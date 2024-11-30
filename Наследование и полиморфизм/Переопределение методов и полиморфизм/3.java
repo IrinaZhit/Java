232b021864bb602070f942985c19ce08ed50926c
IrinaZhit <165552692+IrinaZhit@users.noreply.github.com>
2024-11-30 22:08:58 +0300

    Update 3.java

--- Наследование и полиморфизм/Переопределение методов и полиморфизм/3.java
+++ Наследование и полиморфизм/Переопределение методов и полиморфизм/3.java
@@ -5,10 +5,18 @@ class Person {
 }
 
 class Student extends Person {
+    @Override // Указание, что метод должен переопределять метод суперкласса
+    public void displayInfo() { // Теперь правильно указано имя метода
+        System.out.println("Это информация о студенте.");
+        }
+    
+    /*
     // Ошибка: неверно указано имя метода
+     @Override
     public void displayInf() { // Пропущена буква "o"
         System.out.println("Это информация о студенте.");
     }
+    */
 }
 
 public class Main {
